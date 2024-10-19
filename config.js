41bea1b8739874cb7d80122477aac352030fab66
HuyEVIL-bit <Huydoxuan3333@gmail.com>
2024-10-20 03:57:06 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Nguyễn Hồng Hà';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Dear Nguyễn Hồng Hà';
+const contentCard = 'Chúc cậu có một ngày 20/10 thật ý nghĩa, đầy hạnh phúc và luôn luôn tươi tắn rạng rỡ như những đóa hoa';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
